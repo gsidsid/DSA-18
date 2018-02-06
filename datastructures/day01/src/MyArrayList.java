21bf32ece3f4d62dc289639f49ba57011c28fa3b
Siddharth Garimella <gsidsid@gmail.com>
2018-02-06 18:37:59 -0500

    finally

--- datastructures/day01/src/MyArrayList.java
+++ datastructures/day01/src/MyArrayList.java
@@ -1,42 +1,151 @@
+import java.util.Arrays;
+
 public class MyArrayList {
     private Cow[] elems;
     private int size;
 
-    // TODO: Runtime: O(?)
+
+    // TODO: Runtime: O(1):idk
     public MyArrayList() {
         // TODO
+        elems = new Cow[1]; // so when it doubles it actually doubles?
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1):idk
     public MyArrayList(int capacity) {
         // TODO
+        elems = new Cow[capacity];
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1):as the array expands, the array is less likely to need to change in size
     public void add(Cow c) {
         // TODO
+        if(size == elems.length) {
+            Cow[] internalBuffer = new Cow[elems.length*2];
+            System.arraycopy(elems,0,internalBuffer,0,elems.length);
+            elems = internalBuffer;
+        }
+        elems[size] = c;
+        size++;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1):retrieving primitive variable from stack
     public int size() {
         // TODO
-        return -1;
+        return size;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1):indexed retrieval of object reference from array
     public Cow get(int index) {
         // TODO
-        return null;
+        if(index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return elems[index];
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N) if not last element else O(1):Equal to number of elements in leftward shift post-deletion
     public Cow remove(int index) {
         // TODO
-        return null;
+        if(index >= size  || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Cow a = elems[index];
+        if(size < elems.length/4) {
+            Cow[] internalBuffer = new Cow[elems.length/2];
+            System.arraycopy(elems,0,internalBuffer,0,elems.length/2);
+            elems = internalBuffer;
+        }
+
+        for(int i = index+1; i < size; i++) {
+            elems[i-1] = elems[i]; // leftward shift
+        }
+
+        size--;
+        return a;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N):System.arraycopy
     public void add(int index, Cow c) {
         // TODO
+        if(index >= size  || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if(size == elems.length) {
+            Cow[] internalBuffer = new Cow[elems.length*2];
+            System.arraycopy(elems,0,internalBuffer,0,elems.length);
+            elems = internalBuffer;
+        }
+        for(int i = size; i > index; i--) {
+            elems[i] = elems[i-1]; // rightward shift
+        }
+        elems[index] = c;
+        size++;
+    }
+
+    public static void main(String[] args) {
+
+        //why is testing not working??
+
+        Cow DELILAH = new Cow("Delilah", 10, "blue");
+        Cow GEORGY = new Cow("Georgy", 8, "green");
+        Cow LILY = new Cow("Lily", 4, "yellow");
+        Cow JIMY = new Cow("Jimi", 13, "black");
+        Cow JAQUAN = new Cow("Jaquan", 10, "transparent");
+        Cow LEGOS = new Cow("Legos", 400, "rainbow");
+
+        MyArrayList cows = new MyArrayList(4);
+
+        // add
+        System.out.println("Testing add(c): Adding 4 cows");
+        cows.add(DELILAH);
+        cows.add(GEORGY);
+        cows.add(LILY);
+        cows.add(JIMY);
+
+        // size
+        System.out.println("    Testing size:");
+        System.out.println("    " + cows.size());
+        System.out.println("");
+
+        // get
+        System.out.println("Testing get()");
+        System.out.println("EXPECTED GEORGY, GOT " + cows.get(1).name);
+        System.out.println("");
+
+        // remove
+        System.out.println("Testing remove()");
+        Cow a = cows.remove(2);
+        System.out.println("EXPECTED TO REMOVE LILY, GOT " + a.name);
+        System.out.println(cows.size());
+        System.out.println(cows.get(0).name);
+        System.out.println(cows.get(1).name);
+        System.out.println(cows.get(2).name);
+        System.out.println("");
+
+        System.out.println("Testing add(i,c): Adding LILY to index 2");
+        cows.add(2,LILY);
+        System.out.println(cows.size());
+        System.out.println(cows.get(0).name);
+        System.out.println(cows.get(1).name);
+        System.out.println(cows.get(2).name);
+        System.out.println(cows.get(3).name);
+        System.out.println("");
+
+        System.out.println("Testing resizeCows...");
+        MyArrayList resizeCows = new MyArrayList();
+        for (int i = 0; i < 1000; i++) {
+            String name = "Cow" + i;
+            int age = i;
+            String color = "Color" + i;
+            resizeCows.add(new Cow(name,age,color));
+        }
+        Cow findCow = new Cow("Cow996", 996, "Color996");
+        System.out.println("");
+        if(resizeCows.get(996).equals(findCow)) {
+            System.out.println("Cow 996 found.");
+        }
     }
 }
--- datastructures/day03/src/Problems.java
+++ datastructures/day03/src/Problems.java
@@ -14,20 +14,94 @@ public class Problems {
 
     public static List<Integer> removeKDigits(int[] A, int k) {
         // TODO: your code here
-        // For now, return a List that's correct size, but contains only 0s
         List<Integer> l = new LinkedList<>();
-        for (int i = 0; i < A.length - k; i++) l.add(0);
+        for (int i = 0; i < A.length; i++) {
+            l.add(A[i]);
+        }
+        for (int i = 0; i < l.size()-1; i++) {
+                if(l.get(i) > l.get(i+1) && k > 0) {
+                    l.remove(i);
+                    k--;
+                    i=0;
+                }
+        }
+        while(k > 0) {
+            l.remove(l.indexOf(Collections.max(l)));
+            k--;
+        }
         return l;
     }
 
     public static boolean isPalindrome(Node n) {
         // TODO: your code here
-        return false;
+
+        Node lf = n;
+        Node ls = n;
+        Node midpoint = null;
+        Node reverse_pointer = null;
+        Node even_pointer = null;
+        Node odd_pointer = null;
+
+        if(n == null || n.next == null) {
+            return true;
+        }
+
+            while(lf.next!=null && lf.next.next!=null){
+                lf = lf.next.next;
+                ls = ls.next;
+            }
+
+            midpoint = ls.next;
+            ls.next = null;
+            even_pointer = midpoint;
+            odd_pointer = even_pointer.next;
+
+            while(even_pointer!=null && odd_pointer!=null){
+                Node temp = odd_pointer.next;
+                odd_pointer.next = even_pointer;
+                even_pointer = odd_pointer;
+                odd_pointer = temp;
+            }
+
+            midpoint.next = null;
+
+            if(odd_pointer == null) {
+                reverse_pointer = even_pointer;
+            } else {
+                reverse_pointer = odd_pointer;
+            }
+            Node forward = n;
+            while(reverse_pointer!=null){
+                if(reverse_pointer.val != forward.val) {
+                    return false;
+                }
+                reverse_pointer = reverse_pointer.next;
+                forward = forward.next;
+            }
+        return true;
     }
 
     public static String infixToPostfix(String s) {
         // TODO
-        return null;
+
+        String sh = "";
+        Stack<Character> ops = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == ' ' || Character.isDigit(c)) {
+                sh += c;
+                sh = sh.trim();
+            } else if ( c == '+' || c == '-' || c == '/' || c == '*') {
+                ops.push(c);
+            } else if (c == ')') {
+                sh += ops.pop();
+            }
+        }
+
+        return sh.replaceAll(".(?=.)", "$0 ");
     }
+
+
 
 }
